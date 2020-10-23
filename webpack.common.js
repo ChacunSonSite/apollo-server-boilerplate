cc3afc07b45a1fa2ec79f585fecfce41e34507d6
Juan Pablo Aguilar-Lliguin <7308580+chefjuanpi@users.noreply.github.com>
2020-10-23 03:49:10 -0400

    install and configure bundle analyser

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,29 @@
 const path = require('path');
+const { argv } = require('yargs');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
-  output: {
-    filename: 'server.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  target: 'node',
+module.exports = env => {
+  console.log('analize: ', env.a); // 'local'
+  const plugins = []
+  if (env.a) {
+    plugins.push(
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        defaultSizes: 'gzip',
+        generateStatsFile: true,
+        openAnalyzer: true,
+        reportFilename: path.resolve(`../build-analysis/index.html`),
+        statsFilename: path.resolve(`../build-analysis/results.json`),
+      })
+    );
+  }
+
+  return {
+    output: {
+      filename: 'server.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    target: 'node',
+    plugins
+  }
 };
