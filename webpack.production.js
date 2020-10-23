b0f738184ab5c330bf43f8d3e5a7e26ef66a0475
Juan Pablo Aguilar-Lliguin <7308580+chefjuanpi@users.noreply.github.com>
2020-10-23 03:49:44 -0400

    add webpack configuration for production

---
+++ webpack.production.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { merge } = require('webpack-merge');
+const nodeExternals = require('webpack-node-externals');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = env => {
+  const common = require('./webpack.common.js')(env);
+
+  return merge(common, {
+    devtool: 'source-map',
+    entry: [path.join(__dirname, 'src')],
+    externals: [nodeExternals({})],
+    mode: 'production',
+    plugins: [new CleanWebpackPlugin()]
+  });
+}
