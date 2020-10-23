24517129110566bf0bd2e8afab46e391710f18a2
Juan Pablo Aguilar-Lliguin <7308580+chefjuanpi@users.noreply.github.com>
2020-10-23 13:20:24 -0400

    update user schema and use creation for create first SUPER-ADMIN

--- src/schema/user/index.js
+++ src/schema/user/index.js
@@ -1,3 +1,5 @@
+import consola from "consola";
+
 export const UserResolvers = {
   Query: {
     users: (_, args = '', { User }) => {
@@ -22,12 +24,19 @@ export const UserResolvers = {
         throw new Error('User not exist');
       }
     },
-    async signupUser (_, { username, email, password, role }, { User, Role }) {
+    async signupUser (_, { username, email, password, role = 'USER' }, { User, Role }) {
+      // TODO: securize account creation, now any one can be super-admin or Admin. Apply connection verification,
+      // Only SUPER-ADMIN should be capable to create other SUPER-ADMIN,
+      // SUPER-ADMIN and ADMIN can create other ADMIN or USER
+      // when USER is connect don't read role parameter.
+
       if (await User.findOne({ username })) {
         throw new Error('User already exist');
       }
-
-      const roleDB = await Role.findOne({ name: role });
+      var roleDB = await Role.findOne({ name: role });
+      if (await User.estimatedDocumentCount({}) < 1) {
+        roleDB = await Role.findOne({ name: 'SUPER-ADMIN' });
+      }
 
       if (
         roleDB &&
