a4e2634da284f510ae1f8aedfb5998cb476a974b
chefjuanpi <7308580+chefjuanpi@users.noreply.github.com>
2020-10-28 17:03:20 -0400

    update keys seed to verify keys before add extra

--- src/db/seeds/keys.js
+++ src/db/seeds/keys.js
@@ -2,21 +2,24 @@
   This seed will create 20 private PEM key and store it on the database.
   the keys will used randomly for sign the JWTs.
 */
-
 import { generateKeyPairSync } from 'crypto';
 import { Key } from '../models/Key';
 
-if (Key.estimatedDocumentCount() < 20) {
-  const Keys = []
-  for (let i = 0; i < 20; i++) {
-    const { privateKey } = generateKeyPairSync('rsa', {
-      modulusLength: 4096,  // the length of your key in bits
-      privateKeyEncoding: {
-        type: 'pkcs8',      // recommended to be 'pkcs8' by the Node.js docs
-        format: 'pem',
-      }
-    });
-    Keys.push({ key: privateKey });
+(async () => {
+  if (await Key.estimatedDocumentCount() < 20) {
+    consola.success("inside key seeds");
+    const Keys = []
+    for (let i = 0; i < 20; i++) {
+      const { privateKey } = generateKeyPairSync('rsa', {
+        modulusLength: 4096,  // the length of your key in bits
+        privateKeyEncoding: {
+          type: 'pkcs8',      // recommended to be 'pkcs8' by the Node.js docs
+          format: 'pem',
+        }
+      });
+      Keys.push({ key: privateKey });
+    }
+    Key.insertMany(Keys);
   }
-  Key.insertMany(Keys);
-}
+})()
+
